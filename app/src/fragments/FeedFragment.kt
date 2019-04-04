3e45decf9a187fa4fd9a1b504d29bcc26e45f201
Claudiu Iordache <claudiu.iordache@hotmail.com>
2019-04-05 00:20:11 +0300

    Show snackbar when new item is posted.

--- app/src/fragments/FeedFragment.kt
+++ app/src/fragments/FeedFragment.kt
@@ -4,8 +4,10 @@ import adapters.resources.ResourcesRecyclerAdapter
 import android.app.AlertDialog
 import android.content.Context
 import android.os.Bundle
+import android.support.design.widget.Snackbar
 import android.support.v4.app.Fragment
 import android.support.v7.widget.LinearLayoutManager
+import android.support.v7.widget.RecyclerView
 import android.text.InputType
 import android.view.LayoutInflater
 import android.view.View
@@ -26,11 +28,11 @@ import ro.uaic.info.ipfs.R
 import services.ipfsDaemon
 import utils.RVEmptyObserver
 import java.io.IOException
-import java.util.function.Consumer
-import java.util.stream.Collectors
 
 
 class FeedFragment : Fragment() , AnkoLogger {
+
+
     interface FeedFragmentListener {
         fun feedFragmentOnAddTextPressed(fragment: FeedFragment)
         fun feedFragmentOnAddFilePressed(fragment: FeedFragment)
@@ -40,7 +42,9 @@ class FeedFragment : Fragment() , AnkoLogger {
     private var mContext: Context? = null
 
     var delegate: FeedFragmentListener? = null
+
     // UI
+    private lateinit var snackBar: Snackbar
     private lateinit var linearLayoutManager: LinearLayoutManager
     private lateinit var adapter: ResourcesRecyclerAdapter
 
@@ -65,6 +69,7 @@ class FeedFragment : Fragment() , AnkoLogger {
 
     override fun onActivityCreated(savedInstanceState: Bundle?) {
         super.onActivityCreated(savedInstanceState)
+        snackBar = Snackbar.make(feedConstraintLayout , R.string.feed_new_items_text , Snackbar.LENGTH_LONG)
         setupRecyclerView()
         setupActionBtn(notImplemented)
         setupConfigBtn()
@@ -73,6 +78,15 @@ class FeedFragment : Fragment() , AnkoLogger {
 
     fun add(resource: IIpfsResource) {
         adapter.add(resource)
+        val layoutManager = recyclerView.layoutManager as LinearLayoutManager
+        val visiblePosition = layoutManager.findFirstVisibleItemPosition()
+        if (visiblePosition > 0) {
+            snackBar.setAction(R.string.feed_new_items_refresh) {
+                snackBar.dismiss()
+                layoutManager.smoothScrollToPosition(recyclerView, RecyclerView.State(), 0)
+            }
+            snackBar.show()
+        }
     }
 
     private fun setupRecyclerView() {
