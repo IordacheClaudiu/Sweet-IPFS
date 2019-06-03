012fa1e98067db54af1b0b91184b01ec1830e861
Claudiu Iordache <claudiu.iordache@hotmail.com>
2019-06-03 21:55:10 +0300

    Use new AWS Rekognition keys

--- nodejs/app.js
+++ nodejs/app.js
@@ -1,4 +1,4 @@
-require('dotenv').config();
+require("dotenv").config();
 
 // IPFS Setup
 const ipfsClient = require("ipfs-http-client");
@@ -104,22 +104,23 @@ async function processResource(cid) {
 
     // 2. Get image IPFS hash
     const ipfsImageHash = ipfsImageJSON.file.hash;
-    console.log("Image CID: " + ipfsImageHash + ".");
+    console.log("[IPFS] Image CID: " + ipfsImageHash + ".");
 
     // 3. Load image
     const imageBinary = await ipfsCatStream(ipfsImageHash);
-    console.log("Image binary loaded.");
+    console.log("[IPFS] Image binary loaded.");
 
     // 4. AWS Rekognition - detect labels
     const detection = await processImage(imageBinary);
+    console.log("[AWS] Rekognition succedeed");
     ipfsImageJSON.file.detection = detection;
 
     // 5. AES Encryption
     var iv = forge.random.getBytesSync(16);
     var ivEncoded = forge.util.encode64(iv);
-    var salt = forge.random.getBytesSync(8);  
+    var salt = forge.random.getBytesSync(8);
     var aesKey = forge.pkcs5.pbkdf2("password", salt, 1000, 16);
-    console.log("Generated AES Key: " + aesKey);
+    console.log("[AES] Generated AES Key: " + aesKey);
     var cipher = forge.cipher.createCipher("AES-CBC", aesKey);
     cipher.start({ iv: iv });
     var input = forge.util.createBuffer(JSON.stringify(ipfsImageJSON), "utf8");
@@ -127,11 +128,11 @@ async function processResource(cid) {
     cipher.finish();
     var aesEncrypted = cipher.output.getBytes();
     var aesEncryptedEncoded = forge.util.encode64(aesEncrypted);
-
+    console.log("[AES] Encryption succeeded")
     // 6. Add AES encrypted to IPFS
     var addResult = await ipfs.add(Buffer.from(aesEncryptedEncoded));
     var aesCipherCID = addResult[0].hash;
-    console.log("Added Encrypted CID: " + aesCipherCID);
+    console.log("[IPFS]Added Encrypted CID: " + aesCipherCID);
 
     // 7. RSA Encrypt
     var publicKey = ipfsImageJSON.peer.publicKey;
@@ -140,10 +141,11 @@ async function processResource(cid) {
       publicKey +
       "\n" +
       "-----END PUBLIC KEY-----";
+    console.log("[RSA] Using key: \n" + public);
     var rsaPublicKey = forge.pki.publicKeyFromPem(public);
-    var rsaEncrypted = rsaPublicKey.encrypt(aesKey, 'RSA-OAEP');
-    var rsaEncryptedEncoded =  forge.util.encode64(rsaEncrypted);
-    
+    var rsaEncrypted = rsaPublicKey.encrypt(aesKey, "RSA-OAEP");
+    var rsaEncryptedEncoded = forge.util.encode64(rsaEncrypted);
+    console.log("[RSA] Encryption succeeded");
 
     // 8. Publish new entry
     var ipnsEntry = {
@@ -154,21 +156,37 @@ async function processResource(cid) {
     var peerAddress = ipfsImageJSON.peer.addresses[0];
     var peerAddressesComps = peerAddress.split("/");
     var topic = peerAddressesComps[peerAddressesComps.length - 1];
-    console.log("Publish to: " + topic);
     await ipfs.pubsub.publish(topic, Buffer.from(JSON.stringify(ipnsEntry)));
-    return "IPFS success";
+    console.log("[IPFS ]Publish to: " + topic + " succeeded");
+    return "Success";
   } catch (error) {
     return error;
   }
 }
 
 function main() {
-  processResource("QmUg8HT2ZHDU6wzsznVoecfQTjUEEQJAwMgDAbgu2kGKs3")
-    .then(function(result) {
-      console.log(result);
-    })
-    .catch(function(error) {
-      console.log(error);
-    });
+  const topic = "calamities";
+  ipfs.pubsub.subscribe(
+    topic,
+    msg => {
+      const hash = msg.data.toString();
+      console.log("-----------------------------------------");
+      processResource(hash)
+        .then(function(result) {
+          console.log(result);
+          console.log("-----------------------------------------");
+        })
+        .catch(function(error) {
+          console.log(error);
+          console.log("-----------------------------------------");
+        });
+    },
+    err => {
+      if (err) {
+        return console.error(`failed to subscribe to ${topic}`, err);
+      }
+      console.log(`subscribed to ${topic}`);
+    }
+  );
 }
 main();
